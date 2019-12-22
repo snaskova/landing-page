f5f143c6a0e31f935a01c345042dfee8a5a7ba7f
Stef Naskova <snaskova094@gmail.com>
2019-12-22 22:15:09 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+});
+
