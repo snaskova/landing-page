7f6a00169f4ead23e598f100178c43aab4945109
Stef Naskova <snaskova094@gmail.com>
2019-12-23 00:52:41 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,22 @@ $('.owl-carousel').owlCarousel({
     items: 1,
     loop: true,
     dots: false,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
     nav: true,
     navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
 });
 
+
+$('#toggle').on('click', () => {
+    $('#toggle').toggleClass('active');
+    $('.nav_ul').toggleClass('open');
+})
+
+$('.nav_link').on('click', function(){
+  if($(window).width() < 768){
+            $('#toggle').click();
+  }
+
+  });
